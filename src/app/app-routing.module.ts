63f5ff016dfcb6ff21ec586cf59d9bb9e97d5cf4
Aster07102000 <aster.r@codezen.in>
2025-05-29 18:16:24 +0530

    29-05-2025

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,19 +4,23 @@ import { Page404Component } from './page404/page404.component';
 import { AppComponent } from './app.component';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: AppComponent,
-    pathMatch: 'full'
-  },
-  {
-    path: '**',
-    component: Page404Component
-  }
+    {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        component: AppComponent,
+    },
+    {
+        path: '**',
+        component: Page404Component
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
